15c180fdffcb63a908c23b1ff25981990b766c8f
rajithst <rajiththennakoonshady@gmail.com>
2016-09-17 17:56:32 +0530

    course cordinator partially

--- components/course_head.php
+++ components/course_head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>CSC</title>
     <link href="../public/css/custom.css" rel="stylesheet"/>
     <link href="../public/dist/css/newCourseCoordinator.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
