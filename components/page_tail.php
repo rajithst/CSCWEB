eeeb3087881aeacafb26dbc812ad313184361803
rajithst <rajiththennakoonshady@gmail.com>
2016-11-08 01:47:45 +0530

    changee

--- components/page_tail.php
+++ components/page_tail.php
@@ -1,6 +1,28 @@
+<footer lass="container-fluid" style="
+    position:fixed;
+   left:0px;
+   bottom:0px;
+   height:40px;
+   width:100%;
+">
+    <div class = "container-fluid">
+        <div class="row">
+            <div col-md-5 class="footer-content">
+                <ul class="footer-nav">
 
-<script src="../public/plugins/jQuery/jquery.js"></script>
+                    <center>Computing Service Center - University of Colombo School of Computing</center>
+
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <script src="../public/js/bootstrap.js"></script>
-<script src="../public/plugins/wysiwig/bootstrap3-wysihtml5.all.min.js"></script>
-</body>
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
+
+
+
+</body
 </html>
---
+++ staff/comp/navbar.php
@@ -0,0 +1,112 @@
+<style>
+
+
+    .navbar-login
+    {
+        width: 305px;
+        padding: 10px;
+        padding-bottom: 0px;
+    }
+
+    .navbar-login-session
+    {
+        padding: 10px;
+        padding-bottom: 0px;
+        padding-top: 0px;
+    }
+
+    .icon-size
+    {
+        font-size: 87px;
+    }
+</style>
+
+<nav class="navbar navbar-inverse" id="myNavbar" >
+    <div class="container-fluid" >
+        <div class="navbar-header" >
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" >
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#"><img src="../public/dist/img/system/csclogo.png" style="width:170px; height:50px;" ></a>
+        </div>
+        <div class="collapse navbar-collapse"  >
+
+            <ul class="nav navbar-nav navbar-right" id="navbar_txt" >
+                <li>
+                    <a href="index.php" style="color:white;" class="glyphicon glyphicon-home"> Home</a>
+                </li>
+                <li class="dropdown" style="margin-right:4px" >
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:white;padding-top:18px;" >
+                        <span class="glyphicon glyphicon-globe"></span>
+                        Notifications
+                        <span class="caret">
+					</span>
+                    </a>
+                    <ul class="dropdown-menu multi-level dropdown-color" role="menu" aria-labelledby="dropdownMenu" id="tasks_dropdown">
+                        <li><a tabindex="-1" href="" id="task_txt">Emails</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:white;padding-top:18px;">
+                        <span class="glyphicon glyphicon-list-alt"> Reports</span>
+                        <span class="caret">
+						</span>
+                    </a>
+                    <ul class="dropdown-menu multi-level dropdown-color" role="menu" aria-labelledby="dropdownMenu" id="tasks_dropdown">
+                        <li><a tabindex="-1" href="edit_rep.php" id="task_txt">Edit Report</a></li>
+                        <li><a tabindex="-1" href="report_gen.php" id="task_txt">Generate report</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="select_course_reg.php" style="color:white;padding-top:18px;"><span class="glyphicon glyphicon-edit"></span> Registration</a>
+                </li>
+
+                <ul class="nav navbar-nav navbar-right" >
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="glyphicon glyphicon-user"></span> 
+                            <strong><?php echo $staff_data['first_name']; ?></strong>
+                            <span class="glyphicon glyphicon-chevron-down"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <div class="navbar-login">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <p class="text-center">
+                                                <span class="glyphicon glyphicon-user icon-size"></span>
+                                            </p>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <p class="text-left"><strong><?php echo $staff_data['first_name']. " ". $staff_data['last_name']; ?></strong></p>
+                                            <p class="text-left small"><?php echo $staff_data['email']; ?></p>
+                                            <p class="text-left">
+                                                <a href="#" class="btn btn-primary btn-block btn-sm">See Profile</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="divider"></li>
+                            <li>
+                                <div class="navbar-login navbar-login-session">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <p>
+                                                <a href="logout.php" class="btn btn-danger btn-block">Logout</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+
+            </ul>
+        </div>
+    </div>
+</nav>
