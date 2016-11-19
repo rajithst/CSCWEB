31dbc160ebd2af5d5f64862992d6f11558163fb7
Humairah Dahlan <humairahdahlan19@gmail.com>
2016-11-19 21:12:01 +0530

    navi

--- cscco/attendence.php
+++ cscco/attendence.php
@@ -23,15 +23,15 @@ include '../components/cscordinator_head.php'; ?>
 <body>
 
 <?php include "comp/navbar.php"; ?>
-<div class="nabbar clearfix" id="path">
-        <ol class="breadcrumb">
-            <li><a href="index.php">Home</a>
-            <span class="glyphicon glyphicon-triangle-right"></span></li>
-            <li><a href="report.php"> Reports</a>
-            <span class="glyphicon glyphicon-triangle-right"></span></li>
-            <li><a href="income.php">Attendence</a>
-        </ol>
-    </div>
+<ul class="breadcrum">
+        <li class="completed"><a href="index.php">HOME</a></li>
+        <li class="completed"><a href="">Reports</a></li>
+        <li class="active"><a href="attendence.php">Attendence</a></li>
+
+    </ul>
+
+    </br>
+    
 
     <div class="container-fluid">
         <div class="row centered-form">
