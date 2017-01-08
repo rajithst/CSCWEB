317c603cbc405351a5fb94675906789eaf357f8f
rajithst <rajiththennakoonshady@gmail.com>
2017-01-08 15:08:35 +0530

    a

--- core/base.php
+++ core/base.php
@@ -4,14 +4,14 @@ $con = mysqli_connect('localhost','root','rajith','csc');
 $current_file= explode('/',$_SERVER['SCRIPT_NAME']);
 $current_file=end($current_file);
 
-if ($current_file != 'index.php') {
+
     function logged_in()
     {
 
         return (isset($_SESSION['id'])) ? true : false;
 
     }
-}
+
 
 
 
