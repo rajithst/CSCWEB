3d89b29e89b4e2702b5aedc81c46bdb468a225ea
rajithst <rajiththennakoonshady@gmail.com>
2017-01-15 23:44:30 +0530

    a

--- core/function/admin.php
+++ core/function/admin.php
@@ -13,6 +13,7 @@ function login( $con,$email, $password) {
 	$user_id  = user_id_from_email($con,$email);
 	$password = md5($password);
 	$sql      = "SELECT * FROM adminusers WHERE email= '$email' AND password='$password'";
+
 	$query    = mysqli_query($con, $sql);
 	$result   = mysqli_num_rows($query);
 
